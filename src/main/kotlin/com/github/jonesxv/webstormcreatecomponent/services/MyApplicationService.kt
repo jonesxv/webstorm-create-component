dee83d568b5e21d499fed671d848fc0f37f4b384
GitHub Action <action@github.com>
2022-04-07 20:52:24 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/jonesxv/webstormcreatecomponent/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.jonesxv.webstormcreatecomponent.services
+
+import com.github.jonesxv.webstormcreatecomponent.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/test/kotlin/com/github/jonesxv/webstormcreatecomponent/MyPluginTest.kt
+++ src/test/kotlin/com/github/jonesxv/webstormcreatecomponent/MyPluginTest.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.jonesxv.webstormcreatecomponent
 
 import com.intellij.ide.highlighter.XmlFileType
 import com.intellij.psi.xml.XmlFile
